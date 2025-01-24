71557d5cc6dade6f0717698cbe79e4fe49db20c6
siva1827 <sivaravinadhan@gmail.com>
2025-01-24 11:09:58 +0530

    Added Admin BarChart

--- src/app/component/admin/admin-barchart/admin-barchart.component.ts
+++ src/app/component/admin/admin-barchart/admin-barchart.component.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CanvasJS, CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 
 @Component({
   selector: 'app-admin-barchart',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,CommonModule,
+    CanvasJSAngularChartsModule],
   templateUrl: './admin-barchart.component.html',
   styleUrl: './admin-barchart.component.css'
 })
-export class AdminBarchartComponent {
+export class AdminBarchartComponent implements OnInit{
+  chartOptions: any = {
+    title: {
+      text: "Sales Over the Years"
+    },
+    data: []
+  };
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchChartData();
+  }
+
+  ngAfterViewInit() {
+    // Remove the CanvasJS credit link after the chart is rendered
+    const creditElement = document.querySelector('.canvasjs-chart-credit');
+    if (creditElement) {
+      creditElement.remove();
+    }
+  }
+
+  fetchChartData() {
+    this.http.get<any>('http://localhost:9080/chart-data').subscribe(
+      (data) => {
+        this.chartOptions = {
+          creditHref:'',
+          creditText:"",
+          title: {
+            text: "Sales Over the Years"
+          },
+          axisX: {
+            gridThickness: 0, // Disable grid lines for the X-axis
+            //tickLength: 0 // Optional: Remove tick marks on the X-axis
+          },
+          axisY: {
+            gridThickness: 0, // Disable grid lines for the Y-axis
+            tickLength: 0 // Optional: Remove tick marks on the Y-axis
+          },
+          
+          data: [
+            {
+              type: "column",
+              dataPoints: data.sales.map((sale: any) => ({
+                label: sale.year.toString(),
+                y: sale.amount
+              }))
+            }
+          ]
+        };
+  
+        // Trigger chart rendering manually
+        setTimeout(() => {
+          if (CanvasJS) {
+            CanvasJS.Chart.prototype.render.call(this.chartOptions);
+          }
+        }, 0);
+      },
+      (error) => {
+        console.error('Error fetching chart data:', error);
+      }
+    );
+  }
 
 }
--- src/app/component/admin/home-admin/home-admin.component.ts
+++ src/app/component/admin/home-admin/home-admin.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../side-bar/side-bar.component';
 import { StatBoxesComponent } from "../stat-boxes/stat-boxes.component";
+import { AdminBarchartComponent } from '../admin-barchart/admin-barchart.component';
 
 @Component({
   selector: 'app-home-admin',
   templateUrl: './home-admin.component.html',
   styleUrls: ['./home-admin.component.css'],
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent, StatBoxesComponent] 
+  imports: [HeaderComponent, SidebarComponent, StatBoxesComponent,AdminBarchartComponent] 
 })
 export class HomeAdminComponent {
   // Your logic here
